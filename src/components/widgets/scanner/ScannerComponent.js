f8dfe4093e09c1391b56fb8a36c6c15c7c126e39
hashinika <hashinikam123@gmail.com>
2018-12-08 11:41:29 +0530

    Add scanner component. camera setup and get isbn complete.

--- src/components/widgets/scanner/ScannerComponent.js
+++ src/components/widgets/scanner/ScannerComponent.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+import { RNCamera } from 'react-native-camera';
 
 export default class ScannerComponent extends Component {
   
   constructor(props){
     super(props);
+    this.state ={
+      barcode: ''
+    }
   }
   
   componentDidMount(){
-    this.props.testLogin(2);
+  
   }
+  
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
   
   render() {
     return (
-      <Container>
-        <View>
-          <Text>Scanner Component</Text>
+      <View style={styles.container}>
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+            onGoogleVisionBarcodesDetected={({ barcodes }) => {
+              console.log(barcodes);
+              this.setState({
+                barcode:barcodes[0].data
+              });
+            }}
+          />
+          <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+            <TouchableOpacity
+              onPress={this.takePicture.bind(this)}
+              style = {styles.capture}
+            >
+              <Text style={{fontSize: 14}}>{this.state.barcode}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </Container>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
+});
